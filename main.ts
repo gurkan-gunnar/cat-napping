6690683d849e403f89ac66ff3388a94f257bb803
gurkan-gunnar <131518068+gurkan-gunnar@users.noreply.github.com>
2023-04-22 10:30:17 +0200

    Initiala filer för MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    logging = !(logging)
+    if (logging) {
+        basic.showIcon(IconNames.Target)
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.UntilDone)
+    } else {
+        basic.clearScreen()
+    }
+})
+let logging = false
+logging = false
+basic.forever(function () {
+	
+})
+loops.everyInterval(20000, function () {
+    if (logging) {
+        datalogger.log(
+        datalogger.createCV("temp", input.temperature()),
+        datalogger.createCV("light", input.lightLevel())
+        )
+    }
+})
